ea38ef6bef6b5cddf1fb36b2440e16ffa030cd0e
walidbtz7 <walidboutzougarte@gmail.com>
2023-08-05 17:54:07 +0100

    fix something

--- srcs/mariaDB/mariadb_setup.sh
+++ srcs/mariaDB/mariadb_setup.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-set -e
 
-# Start the MariaDB server to perform initial setup tasks
-mysqld &
+# Initialize database
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo 'Initializing database'
+    mysqld --initialize-insecure
+    echo 'Database initialized'
 
-# Wait for the server to be ready
-while ! mysqladmin ping --silent; do
-    sleep 1
-done
+    echo 'Starting MariaDB'
+    mysqld_safe --datadir='/var/lib/mysql' &
 
-# Set up the database and user
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -uroot -e "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -uroot -e "GRANT ALL ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+    echo 'Waiting for MariaDB to start'
+    for i in {30..0}; do
+        if echo 'SELECT 1' | mysql -uroot &> /dev/null; then
+            break
+        fi
+        echo -n '.'
+        sleep 1
+    done
+    if [ "$i" = 0 ]; then
+        echo >&2 'MariaDB start failed.'
+        exit 1
+    fi
 
-# Wait for a few more seconds to ensure the setup tasks are completed
-sleep 5
+    if [ "$MYSQL_DATABASE" ]; then
+        echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql -u root
 
-# The script exits here, and the container continues running with the entrypoint script.
+        if [ "$MYSQL_USER" -a "$MYSQL_PA    SSWORD" ]; then
+            echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -u root
+            echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -u root
+            echo 'FLUSH PRIVILEGES;' | mysql -u root
+        fi
+    fi
+
+    echo 'Stopping MariaDB'
+    mysqladmin -u root shutdown
+fi
+
+echo 'Starting MariaDB'
+/usr/bin/mysqld_safe --datadir='/var/lib/mysql'
