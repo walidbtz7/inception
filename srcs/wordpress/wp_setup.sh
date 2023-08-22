eb6cfbac2f69dca354f51cc3b01054dc46282a4e
walidbtz7 <walidboutzougarte@gmail.com>
2023-08-22 18:30:51 +0100

    edit .env.example

--- srcs/wordpress/wp_setup.sh
+++ srcs/wordpress/wp_setup.sh
@@ -6,7 +6,7 @@ wp core download --allow-root --path=/var/www/html/wordpress
 
 wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${DB_HOST} --allow-root --path=/var/www/html/wordpress
 
-wp core install --url=${DOMAIN_NAME} --title="insception" --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root --path=/var/www/html/wordpress
+wp core install --url=${DOMAIN_NAME} --title="inception" --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root --path=/var/www/html/wordpress
 
 # Start PHP-FPM.
 exec "$@"
